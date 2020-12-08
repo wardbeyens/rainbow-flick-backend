098c0b7c67b82a58e6a5e2f0e588405cffe0ce50
vranckxrobin <r0703028@student.thomasmore.be>
2020-12-08 10:21:14 +0100

    match model

--- app/models/match.model.js
+++ app/models/match.model.js
@@ -1,5 +1,7 @@
 const Player = require('./player.model');
-
+const Team = require('./team.model');
+const Table = require('./table.model');
+const MatchType = require('./../const/matchType');
 
 module.exports = mongoose => {
     const Match = mongoose.model(
@@ -12,12 +14,18 @@ module.exports = mongoose => {
           dateTimeEnd:  Date,
           homeTeam:  { type: Team, required: true },
           awayTeam:  { type: Team, required: true },
-          players: { type: Player[]}
-
+          players: { type: [Player]},
+          scoreHome : Number,
+          scoreAway : Number,
+          table : Table,
+          scoreSubmittedBy : String,
+          scoreValidated:Boolean,
+          requirementsReached:Boolean,
+          matchType:MatchType
         },
         { timestamps: true }
       )
     );
   
-    return Tag;
+    return Match;
   };
